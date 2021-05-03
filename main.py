a9af4c31247985805f4dafab6e4ee98c4b216761
P3lUZa <P3lUZa@users.noreply.github.com>
2021-05-03 00:49:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
-import savepagenow
-
+from requests.exceptions import ConnectionError, TooManyRedirects
 from savepagenow.exceptions import WaybackRuntimeError
+import savepagenow
 
 yahoo_urls = open('yahoo_links.txt', 'r')
 linksx = yahoo_urls.readlines()
 
-#The file to store links
+# The file to store links
 wayback = open('wayback.txt', 'a')
 
-#File for missing links
+# File for missing links
 wayback_errors = open('wayback_errors.txt', 'a')
 
 for line in linksx:
-    #This is to remove the \n at the end of each line (since we are using a text file)
+    # This is to remove the \n at the end of each line (since we are using a text file)
     l = len(line)
-    y_url = line[:l-1]
+    y_url = line[:l - 1]
 
-    #Try to catch errors
-    try:
-        k = savepagenow.capture_or_cache(y_url)
-        print(k[0])
-        r = k[0]
-        wayback.write(r+'\n')
-    #This will handle wayback errors
-    except WaybackRuntimeError as error:
-        print(error)
-        wayback_errors.write(line)
-        wayback_errors.write('\n')
-        print("error en: "+line+'\n')
+    # Try to catch errors
+    x = 0
+    for x in range(4):
+        try:
+            k = savepagenow.capture_or_cache(y_url)
+            print(k[0])
+            r = k[0]
+            wayback.write(r + '\n')
+            break
+        # This will handle wayback errors
+        except WaybackRuntimeError as error:
+            print(error)
+            wayback_errors.write(line)
+            wayback_errors.write('\n')
+            print("error in: " + line + '\n')
+            break
+        except ConnectionError:
+            wayback_errors.write("connection error in: " + line + '\n')
+            print("conection error in: " + line + '\n')
+            x = x + 1
+        except TooManyRedirects:
+            wayback_errors.write("too_many_redirects error in: " + line + '\n')
+            print("too_many_red error in: " + line + '\n')
+            x = x + 1
